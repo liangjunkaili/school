1a06ceffd5cae9a46cc958ef5840befa9106221f
liangjun <jun.liang@magicabc.com.cn>
2018-05-24 16:38:03 +0800

    首页

--- src/components/first.vue
+++ src/components/first.vue
@@ -1,20 +1,85 @@
 <template>
-  <div class="first">
-    <h1>{{msg}}</h1>
+<div class="container">
+  <NavBar style="float:left;margin-top:80px;margin-left:-200px;"/>
+  <div class="first-table">
+    <el-table :data="tableData">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+        align="center">
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
   </div>
+</div>
 </template>
 <script>
+import NavBar from '@/pages/NavBar';
 export default {
   name:'First',
   data () {
     return{
-      msg:'first.vue....................'
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4,
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  },
+  components:{
+    NavBar
   }
 }
 </script>
-<style scoped>
-.first h1{
-  font-size: large;
+<style>
+.first-table{
+  width: 80%;
+  float: right;
+  margin-top:80px;
 }
+
 </style>
--- src/pages/menu.vue
+++ src/pages/menu.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    <h2>woshianniu</h2>
+  <div class="header">
+    <img src="../assets/logo.png" alt="logo" class="logo">
+    <button type="button" class="el-button">
+      <span>退出</span>
+    </button>
     <router-view></router-view>
   </div>
 </template>
+<style>
+.header {
+    width: 100%;
+    height: 63px;
+    padding: 0;
+    margin: 0;
+    background-color: #1bb754;
+}
+.logo{
+  float: left;
+  padding-top:17px;
+  padding-left: 20px;
+}
+.el-button{
+  float: right;
+  padding-top:17px;
+  padding-left: 20px;
+}
+</style>
